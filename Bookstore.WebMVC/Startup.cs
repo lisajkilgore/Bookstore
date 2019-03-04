08f6883d4a78e3aac2ff54a9089fe995ddf9b96d
lisajkilgore <lisajkilgore@gmail.com>
2019-03-04 11:25:39 -0500

    Add project files.

---
+++ Bookstore.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bookstore.WebMVC.Startup))]
+namespace Bookstore.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
