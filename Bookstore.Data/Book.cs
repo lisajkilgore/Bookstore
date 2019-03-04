247d459800fbb31ffd009afd2c1a1145922f0d34
lisajkilgore <lisajkilgore@gmail.com>
2019-03-04 16:57:27 -0500

    data components

---
+++ Bookstore.Data/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Data
+{
+    public enum BookType { Art_Photography, Biographies_Memoirs, Business, Children_s, Cookbook, History, Mystery_Suspense, Political, Religion_Spiritual, Romance, Self_Help, SciFi_Fantasy, Sports_Outdoors, Teen_YoungAdult, Travel}
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [Required]
+        public int OwnerId { get; set; }
+
+        [DefaultValue(true)]
+        public bool IsFiction { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsNewRelease { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsBestSeller { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public BookType TypeOfBook { get; set; }
+    }
+}
---
+++ Bookstore.Data/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Data
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public int BookId { get; set; }
+        public int OwnerId { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Book Book { get; set; }
+    }
+}
