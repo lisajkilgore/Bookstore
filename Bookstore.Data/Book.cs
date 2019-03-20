ca602525dc5071cc56be9fc9088c228bc20817b8
lisajkilgore <lisajkilgore@gmail.com>
2019-03-20 00:09:44 -0400

    description

--- Bookstore.Data/Book.cs
+++ Bookstore.Data/Book.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Bookstore.Data
 {
-    public enum BookType { [Display(Name="Art & Photography")]Art_Photography, [Display(Name = "Biographies & Memoirs")]Biographies_Memoirs, Business, [Display(Name ="Children's")] Children_s, Cookbook, History, [Display(Name = "Mystery & Suspense")]Mystery_Suspense, Political, [Display(Name = "Religion & Spiritual")]Religion_Spiritual, Romance, [Display(Name = "Self-Help")]Self_Help, [Display(Name = "Sci-Fi & Fantasy")]SciFi_Fantasy, [Display(Name ="Sports & Outdorrs")]Sports_Outdoors, [Display (Name="Teen & Young Adult")]Teen_YoungAdult, Travel }
+    public enum BookType { [Display(Name="Art & Photography")]Art_Photography, [Display(Name = "Biographies & Memoirs")]Biographies_Memoirs, Business, [Display(Name ="Children's")] Children_s, Cookbook, History, [Display(Name = "Mystery & Suspense")]Mystery_Suspense, Political, [Display(Name = "Religion & Spiritual")]Religion_Spiritual, Romance, [Display(Name = "Self-Help")]Self_Help, [Display(Name = "Sci-Fi & Fantasy")]SciFi_Fantasy, [Display(Name ="Sports & Outdoors")]Sports_Outdoors, [Display (Name="Teen & Young Adult")]Teen_YoungAdult, Travel }
     public class Book
     {
         [Key]
